d4fae63b76bbdacce20a3a9af7f87bbc48bc8585
Shohan9930 <sshohan9930@gmail.com>
2023-07-30 17:22:35 +0530

    Complete Task 3

---
+++ DemoTask3.java
@@ -0,0 +1,9 @@
+public class DemoTask3 {
+    public static void main(String[] args) {
+        int numSet1=123;
+        int numSet2=456;
+        int numSet3=7890;
+        System.out.printf("\033[34;1m(%s)\033[0m %s-%s \n",numSet1,numSet2,numSet3);
+    }
+    
+}
